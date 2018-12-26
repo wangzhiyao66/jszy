16451c9cfcd8980ef7cf168c1fe86381f8756708
王治尧 <1217167460@qq.com>
2018-12-26 12:56:37 +0800

    代码格式化 ‘单双引号问题’

--- src/app/router/technology/story-detaills/story-detaills.component.ts
+++ src/app/router/technology/story-detaills/story-detaills.component.ts
@@ -1,46 +1,52 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {TechnologyServiceService} from '../technology-service.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TechnologyServiceService } from '../technology-service.service';
 
 @Component({
-    selector: 'app-story-detaills',
-    templateUrl: './story-detaills.component.html',
-    styleUrls: ['./story-detaills.component.scss']
+  selector: 'app-story-detaills',
+  templateUrl: './story-detaills.component.html',
+  styleUrls: ['./story-detaills.component.scss']
 })
 export class StoryDetaillsComponent implements OnInit {
-    detaillId = 0;
-    list: any [] = this.service.storylist;
-    showinfo: any;
+  detaillId = 0;
+  list: any[] = this.service.storylist;
+  showinfo: any;
 
-    constructor(private router: Router,
-                private service: TechnologyServiceService,
-                private activeRoute: ActivatedRoute) {
-        // 监听projectId
-        this.activeRoute.queryParams.subscribe({
-            next: params => {
-                this.detaillId = params.id;
-                this.showData();
-                console.log('id', this.detaillId);
-            }
-        });
-    }
+  constructor(
+    private router: Router,
+    private service: TechnologyServiceService,
+    private activeRoute: ActivatedRoute
+  ) {
+    // 监听projectId
+    this.activeRoute.queryParams.subscribe({
+      next: params => {
+        this.detaillId = params.id;
+        this.showData();
+        console.log('id', this.detaillId);
+      }
+    });
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {}
 
-    // 查找数据
-    showData() {
-        const index = this.list.findIndex(item => Number(item.id) === Number(this.detaillId));
-        this.showinfo = this.list[index];
-    }
-    // 查看其它文章
-    goto(falg: boolean) {
-        const params: number = falg ? (Number(this.detaillId) - 1) : (Number(this.detaillId) + 1 );
-        this.router.navigate(['/technology/story-detaills'],  { queryParams: { id: params} });
-    }
-    // 返回
-    goback() {
-        this.router.navigate(['/technology']);
-    }
-
+  // 查找数据
+  showData() {
+    const index = this.list.findIndex(
+      item => Number(item.id) === Number(this.detaillId)
+    );
+    this.showinfo = this.list[index];
+  }
+  // 查看其它文章
+  goto(falg: boolean) {
+    const params: number = falg
+      ? Number(this.detaillId) - 1
+      : Number(this.detaillId) + 1;
+    this.router.navigate(['/technology/story-detaills'], {
+      queryParams: { id: params }
+    });
+  }
+  // 返回
+  goback() {
+    this.router.navigate(['/technology']);
+  }
 }
