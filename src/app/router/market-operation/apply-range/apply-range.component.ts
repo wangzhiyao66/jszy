679d611a3c6a135529ae6dc3c3560aaca27ce437
王治尧 <1217167460@qq.com>
2019-01-08 17:14:51 +0800

    前三个模块

--- src/app/router/market-operation/apply-range/apply-range.component.ts
+++ src/app/router/market-operation/apply-range/apply-range.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-apply-range',
-  templateUrl: './apply-range.component.html',
-  styleUrls: ['./apply-range.component.scss']
+    selector: 'app-apply-range',
+    templateUrl: './apply-range.component.html',
+    styleUrls: ['./apply-range.component.scss'],
 })
 export class ApplyRangeComponent implements OnInit {
 
-  showlist: { code: string, text: string}[] = [
-    { code: '01' , text: '坚实的中医理论基础，并集光学、工程学、物理学、系统论、红外影像学为一体的创新技术'},
-    { code: '02' , text: '先进的红外医学影像技术在中医领域的成功运用'},
-    { code: '03' , text: '基础临床数据夯实'},
-    { code: '04' , text: '完全自主的知识产权'},
-  ];
+    showlist: { src: string, title: string, text: string, falg: boolean}[] = [
+        {
+            src: '../../../../assets/images/market/range/1.png', title: '亚健康', falg: false,
+            text: '对现代社会广大亚健康群体的检测快速、整体、全面、安全、经济，适合现代人工作快节奏、高效率，只需几分钟，就可以全面了解人体的健康基本状况。'
+        },
+        {
+            src: '../../../../assets/images/market/range/2.png', title: '心脑血管、肿瘤评估', falg: false,
+            text: '对女性乳腺疾病及心脑血管疾病、肿瘤的评估方面有较深入的研究，能够早期发现，并能快速检测及早期干预，是女性乳腺癌、心脑血管疾病筛查的首选。'
+        },
+        {
+            src: '../../../../assets/images/market/range/3.png', title: '早期筛查', falg: false,
+            text: '可广泛应用在呼吸、消化、神经、心脑血管、各系统疾病的早期快速筛查和监测。'
+        },
+        {
+            src: '../../../../assets/images/market/range/4.png', title: '中医健康养生', falg: false,
+            text: '在中医药学领域应用前景广阔，对中医证候和脏腑经络的实质研究能提供全新的影像学手段，目前已开发出中医面部红外全息检诊、脏腑、经络热态矩阵自动化分析软件。'
+        },
+    ];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    /**
+     * 下标
+     * index
+     */
+    esein(index) {
+        this.showlist[index].falg = false;
+    }
+
+    eseout(index) {
+        this.showlist[index].falg = true;
+    }
 }
--- src/app/router/market-operation/range-case/range-case.component.ts
+++ src/app/router/market-operation/range-case/range-case.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RangeCaseComponent implements OnInit {
 
+  storylist: {id: number, url: string, title: string }[] = [
+    {id: 1, url: '../../../../assets/images/market/case/1.png',
+      // tslint:disable-next-line:max-line-length
+      title: '北大医疗健康管理中心：中医体检+保险模式'},
+
+    {id: 2, url: '../../../../assets/images/market/case/2.png',
+      // tslint:disable-next-line:max-line-length
+      title: '宣武中医医院：专科专病模式'},
+    {id: 3, url: '../../../../assets/images/market/case/3.png',
+      // tslint:disable-next-line:max-line-length
+      title: '北京社区健康促进会：家庭医生签约服务模式'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
