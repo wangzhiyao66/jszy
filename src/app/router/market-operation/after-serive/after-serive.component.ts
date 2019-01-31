563e5f0f11b140547d7161ef3f02007ff0c3c8c7
王治尧 <1217167460@qq.com>
2019-01-31 11:13:04 +0800

    优化

--- src/app/router/market-operation/after-serive/after-serive.component.ts
+++ src/app/router/market-operation/after-serive/after-serive.component.ts
@@ -15,11 +15,11 @@ export class AfterSeriveComponent implements OnInit , OnChanges{
   showlist: { src: string, title: string, text: string, falg: boolean}[] = [
     {
       src: '../../../../assets/images/market/after/1.png', title: '保修服务', falg: false,
-      text: '设备免费保修一年，终身维护。'
+      text: '设备免费保修一年，终身维护'
     },
     {
       src: '../../../../assets/images/market/after/2.png', title: '培训服务', falg: false,
-      text: '提供操作使用、报告解读的技术培训。'
+      text: '提供操作使用、报告解读的技术培训'
     },
     {
       src: '../../../../assets/images/market/after/3.png', title: '远程支持', falg: false,
